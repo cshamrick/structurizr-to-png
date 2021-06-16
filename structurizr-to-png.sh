1e0d716741747f606bfadaa7f2017e08b7472d23
Scott Hamrick <shamrick@bellese.io>
2021-06-16 09:44:33 -0500

    fix(structurizr-to-png.sh): cleans up puml files

--- structurizr-to-png.sh
+++ structurizr-to-png.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-ls -altr
-
-/structurizr-to-png/structurizr.sh export -workspace $1 -format plantuml/c4plantuml
-java -jar /structurizr-to-png/plantuml.jar *.puml
+/structurizr-to-png/structurizr.sh export -workspace $1 -format plantuml/c4plantuml -output /tmp
+java -jar /structurizr-to-png/plantuml.jar /tmp/*.puml -o "${PWD}"
